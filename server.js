766e3f33f0fd2ead44531b407851b5e548d4bc8a
fractalswift <matthewthomasmacklin@gmail.com>
2020-04-16 17:55:35 +0100

    changed port from 3000 to dynamic process.env

--- server.js
+++ server.js
@@ -41,6 +41,8 @@ app.post('/imageurl', (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(3000, () => {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    `from app.listen, port is now running on port: ${process.env.PORT}`
+  );
 });
